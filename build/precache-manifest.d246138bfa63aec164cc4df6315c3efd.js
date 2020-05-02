34b8abb64b05cd168a88cc6c45b3e1dbc04a462b
animoffa <60775102+animoffa@users.noreply.github.com>
2020-05-02 22:17:11 +0300

    Add files via upload

---
+++ build/precache-manifest.d246138bfa63aec164cc4df6315c3efd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2afcc521a68e8b950a760d70d801a45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7c4d3aaf20316d5ebda",
+    "url": "/static/css/main.e2f22141.chunk.css"
+  },
+  {
+    "revision": "b38d2c6b5bd34c46d422",
+    "url": "/static/js/2.f9671eda.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f9671eda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7c4d3aaf20316d5ebda",
+    "url": "/static/js/main.9f60f354.chunk.js"
+  },
+  {
+    "revision": "fb114f69e86f18a22817",
+    "url": "/static/js/runtime-main.1ee8b019.js"
+  }
+]);
